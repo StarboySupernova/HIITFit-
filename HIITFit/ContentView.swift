fbc2516a7821b7129f7a45b8106d62fcebcc2fd4
Christian Simbarashe <supernovaonline@outlook.com>
2021-10-05 22:57:20 +0100

    Initial Commit

---
+++ HIITFit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HIITFit
+//
+//  Created by user207077 on 10/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by user207077 on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

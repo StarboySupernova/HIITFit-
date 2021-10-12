1e0e62e4d7c664a923b546090e1e74a8e55b7430
Christian Simbarashe <supernovaonline@outlook.com>
2021-10-12 14:43:31 +0100

    Learnt about SF symbols and modifiers. Also opening Attributes Inspector

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -13,7 +13,26 @@ struct ExerciseView: View {
     let index: Int
     
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                }
+                .font(.title2)
+                
+            }
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+            Image(systemName: "1.circle")
+        }
     }
 }
 

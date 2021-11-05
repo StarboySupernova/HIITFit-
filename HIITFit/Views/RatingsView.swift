720b38cb528d4d61b1fa197045bf023213c7c96a
Christian Simbarashe <supernovaonline@outlook.com>
2021-11-05 01:44:50 +0000

    Learnt about multiple iPad windows, and making sure the value stored with @AppStorage changes reactively on both windows in a multiple window setup. Mostly on iPads

--- HIITFit/Views/RatingsView.swift
+++ HIITFit/Views/RatingsView.swift
@@ -39,19 +39,28 @@ struct RatingsView: View {
         //using index for both the parameter and name of index in string was ambiguous so instead I used StringIndex
     }
     
+    fileprivate func convertRating() {
+        let index = ratings.index(ratings.startIndex, offsetBy: exerciseIndex)
+        let character = ratings[index]
+        rating = character.wholeNumberValue ?? 0
+    }
+    
     var body: some View {
         HStack {
             ForEach(1 ..< maximumRating + 1) { index in
                 Image(systemName: "waveform.path.ecg")
                     .onAppear{
-                        let index = ratings.index(ratings.startIndex, offsetBy: exerciseIndex)
-                        let character = ratings[index]
-                        rating = character.wholeNumberValue ?? 0
+                        convertRating()
                     }
                     .foregroundColor(index > rating ? offColor : onColor)
                     .onTapGesture {
                         updateRating(index: index)
                     }
+                    .onChange(of: ratings){ _ in
+                        convertRating()
+                    }
+                // Here you set up a reactive method that will call convertRating() whenever ratings changes. If you were using only one window, you wouldn’t notice the effect, but multiple windows can now react to the property changing in another window
+                // previously our window was only being redrawn on change of rating State value
             }
         }
         .font(.largeTitle)

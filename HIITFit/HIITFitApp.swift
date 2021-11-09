780a9f08790c865309ac5aeeb8a1d5aadc327bf6
Christian Simbarashe <supernovaonline@outlook.com>
2021-11-09 01:21:37 +0000

    Learnt about property list serialization. PropertyListSerialization is just one way of saving data to disk. You could also use JSON, or Core Data, which manages objects and their persistence. Learnt how to write history data to a plist file and loading it back from thence when needed. We had to convert it to an array type and use data and byte buffers

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -24,7 +24,7 @@ struct HIITFitApp: App {
         }
         _historyStore = StateObject(wrappedValue: historyStore)
         
-        // couldn't use this, historyStore, by dint of being a StateObject property is a get only property. This following code gives an error
+        // couldn't use this, historyStore by dint of being a StateObject property is a get only property. This following code gives an error
         //self.historyStore = historyStore
     }
     
@@ -37,6 +37,12 @@ struct HIITFitApp: App {
                 .onAppear {
                     print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
                 }
+                        /*
+                         Here you use the shared file manager object to list the URLs for the specified directory. There are many significant directories, which you can find in the documentation at https://apple.co/3pTE3U5. Remember that your app is sandboxed, and each app will have its own app directories.
+                         ➤ Build and run in Simulator. The debug console will print out an array of URLs for your Documents directory path in the specified domain. The domain here, userDomainMask, is the user’s home directory.
+                         
+                         */
+                        // on a related note, Erasing all contents and settings creates a completely new app sandbox, so open the path printed in the console if you do that to access the new info.plist
                 .alert(isPresented: $showAlert){
                     Alert(title: Text("History"),
                           message: Text(
@@ -46,12 +52,7 @@ struct HIITFitApp: App {
                                   support@supernovaonline.com
                                 """))
                 }
-            /*
-             Here you use the shared file manager object to list the URLs for the specified directory. There are many significant directories, which you can find in the documentation at https://apple.co/3pTE3U5. Remember that your app is sandboxed, and each app will have its own app directories.
-             ➤ Build and run in Simulator. The debug console will print out an array of URLs for your Documents directory path in the specified domain. The domain here, userDomainMask, is the user’s home directory.
-             
-             */
-            // on a related note, Erasing all contents and settings creates a completely new app sandbox, so open the path printed in the console if you do that to access the new info.plist
+            
         }
     }
 }
--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -50,12 +50,40 @@ class HistoryStore: ObservableObject {
         } else {
             exerciseDays.insert(ExerciseDay(date: today, exercises: [exerciseName]), at: 0)
         }
+        
+        do {
+            try save()
+        } catch {
+            fatalError(error.localizedDescription)
+            //If there’s an error in saving, you crash the app, printing out the string description of your error. This isn’t a great way to ship your app, and you may want to change it later
+        }
+        
         //was for debug purposes and learning to use breakpoints
         //print("History", exerciseDays)
     }
     
     func load () throws {
-        //throw FileError.loadFailure
+        // First set up the URL just as you did with saving the file
+        guard let dataURL = getUrl() else {
+            throw FileError.urlFailure
+        }
+        
+        do {
+            //Read the data file into a byte buffer. This buffer is in the property list format. If history.plist doesn’t exist on disk, Data(contentsOf:) will throw an error. Throwing an error is not correct in this case, as there will be no history when your user first launches your app. You’ll fix this error as your challenge for this chapter.
+            //let data = try Data(contentsOf: dataURL)
+            // we made it check if history.plist exists first, if it doesn't it'll take no action
+            guard let data = try? Data(contentsOf: dataURL) else {return}
+            //Convert the property list format into a format that your app can read
+            let plistData = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+            //When you serialize from a property list, the result is always of type Any. To cast to another type, you use the type cast operator as?. This will return nil if the type cast fails. Because you wrote history.plist yourself, you can be pretty sure about the contents, and you can cast plistData from type Any to the [[Any]] type that you serialized out to file. If for some reason history.plist isn’t of type [[Any]], you provide a fall-back of an empty array using the nil coalescing operator ??.
+            let convertedPlistData = plistData as? [[Any]] ?? []
+            //With convertedPlistData cast to the expected type of [[Any]], you use map(_:) to convert each element of [Any] back to ExerciseDay. You also ensure that the data is of the expected type and provide fall-backs if necessary
+            let exerciseDays = convertedPlistData.map {
+                ExerciseDay(date: $0[1] as? Date ?? Date(), exercises: $0[2] as? [String] ?? [])
+            }
+        } catch {
+            throw FileError.loadFailure
+        }
     }
     
     func getUrl () -> URL? {
@@ -68,6 +96,16 @@ class HistoryStore: ObservableObject {
         
         let plistData = exerciseDays.map{
             [$0.id.uuidString, $0.date, $0.exercises]
+        }
+        
+        do {
+            //You convert your history data to a serialized property list format. The result is a Data type, which is a buffer of bytes
+            let data = try PropertyListSerialization.data(fromPropertyList: plistData, format: .binary, options: .zero)
+            //You write to disk using the URL you formatted earlier.
+            try data.write(to: dataUrl, options: .atomic) //seems like you can try multiple funcs in a single do scope and if any or both throw an error you can coalesce it into 1 compound error
+        } catch {
+            //The conversion and writing may throw an error, which you catch by throwing an error
+            throw FileError.saveFailure
         }
         
         //longer version on using the map(_:) that accepts a closure as an argument and applies that closure to each element of the array and returns a new array with the transformations applied
